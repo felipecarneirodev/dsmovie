d88dcf94ebd9dec095766aee67f6c65dde4605ad
felipecarneirodev <felipecarneirodev@gmail.com>
2022-03-07 17:09:16 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/ProjetoReact/dsmovie/ProjetoReactApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoReact.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ProjetoReact/dsmovie/ProjetoReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoReact.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
