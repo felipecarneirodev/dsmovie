b9b407134cd0efe1e8fc95f575720410bdf7fa42
felipecarneirodev <felipecarneirodev@gmail.com>
2022-03-09 11:26:08 -0300

    Buscando filmes

---
+++ backend/src/main/java/com/ProjetoReact/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.ProjetoReact.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ProjetoReact.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
